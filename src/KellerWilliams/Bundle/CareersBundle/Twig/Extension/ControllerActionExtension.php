1d31b1656d4895b72746dfa112cad1d2d02ca49a
joshteam <joshteam@gmail.com>
2015-07-07 17:01:01 -0500

    added the twig helper for active navigation

---
+++ src/KellerWilliams/Bundle/CareersBundle/Twig/Extension/ControllerActionExtension.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace KellerWilliams\Bundle\CareersBundle\Twig\Extension;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+/**
+ * A TWIG Extension which allows to show Controller and Action name in a TWIG view.
+ *
+ * The Controller/Action name will be shown in lowercase. For example: 'default' or 'index'
+ *
+ */
+class ControllerActionExtension extends \Twig_Extension
+{
+    /**
+     * @var Request
+     */
+    protected $request;
+
+    /**
+     * @var \Twig_Environment
+     */
+    protected $environment;
+
+    public function setRequest(RequestStack $requestStack = null)
+    {
+        $this->request = $requestStack->getCurrentRequest();
+    }
+
+    public function initRuntime(\Twig_Environment $environment)
+    {
+        $this->environment = $environment;
+    }
+
+    public function getFunctions()
+    {
+        return array(
+            'get_controller_name' => new \Twig_Function_Method($this, 'getControllerName'),
+            'get_action_name' => new \Twig_Function_Method($this, 'getActionName'),
+        );
+    }
+
+    /**
+     * Get current controller name
+     */
+    public function getControllerName()
+    {
+        if(null !== $this->request)
+        {
+            $pattern = "#Controller\\\([a-zA-Z]*)Controller#";
+            $matches = array();
+            preg_match($pattern, $this->request->get('_controller'), $matches);
+
+            return strtolower($matches[1]);
+        }
+
+    }
+
+    /**
+     * Get current action name
+     */
+    public function getActionName()
+    {
+        if(null !== $this->request)
+        {
+            $pattern = "#::([a-zA-Z]*)Action#";
+            $matches = array();
+            preg_match($pattern, $this->request->get('_controller'), $matches);
+
+            return $matches[1];
+        }
+    }
+
+    public function getName()
+    {
+        return 'your_own_controller_action_twig_extension';
+    }
+}
